8ed69be1119e46a972645ad24d898627a4dfdd29
Fayjus Salehin <fsalehin@live.com>
2017-05-04 13:34:32 +0300

    Ready to go

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>MyCovers</title>
+  <meta name="author" content="fayjusss">
+
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
